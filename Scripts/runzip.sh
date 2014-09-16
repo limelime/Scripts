b424054352bf34a3db9af75d40c8daaf8da31f52
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-09-16 16:41:08 -0400

    v1.0.

--- Scripts/runzip.sh
+++ Scripts/runzip.sh
@@ -8,32 +8,51 @@
 #	Commit by actually doing the unzip
 #		runzip.sh <path> commit
 
-# color codes:
+# Handling filename with spaces
+SAVEIFS=$IFS
+IFS=$(echo -en "\n\b")
+
+# Get max file length
+###########################
+max_file_length=0
+for filename in $(find ./ -type f | grep "\.zip")
+do
+	if [ "${#filename}" -gt "$max_file_length" ]
+	then
+		max_file_length=${#filename}
+	fi
+done
+full_cmd_length=$((max_file_length*2))
+
+# Color codes:
+###########################
 red='\e[0;31m'
 green='\e[0;32m'
 no_color='\e[0m' # No Color
 
 
-processed=0
-
 # Unziping
-find ./ -type f | grep "\.zip" |
-while read filename          # As long as there is another line to read ...
+###########################
+processed=0
+for filename in $(find ./ -type f | grep "\.zip")
 do
 	dir=$(dirname "$filename")
 	if [ "$2" = "commit" ]
 	then
-		unzip -q -o "$filename" -d "$dir"
 		processed=$((processed+1))
-		if [ "$?" -eq 0 ] # Get the status of command executed.
+		unzip -q -o "$filename" -d "$dir"
+		if [ "$?" -eq 0 ] # Get the status of previously executed command. Unzip must be before this line.
 		then
-			printf "[$filename][${green}OK${no_color}]\n"
+			printf "[%3d][%-${max_file_length}s][${green}OK${no_color}]\n" "$processed" "$filename"
 		else
-			printf "[$filename][${red}NOK${no_color}]\n"
+			printf "[%3d][%-${max_file_length}s][${red}NOK${no_color}]\n" "$processed" "$filename"
 	    fi
 	else
-		echo "unzip -o $filename -d $dir"
+		printf "[%-${full_cmd_length}s]\n" "unzip -o $filename -d $dir"
 	fi
 done
 
-echo "${processed} processed."
+echo -e "${green}${processed} processed.${no_color}"
+
+# Restore $IFS
+IFS=$SAVEIFS
