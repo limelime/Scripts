d9ca48e258e637eeb390d9c11a4a69d99bb2f244
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-09-16 15:48:35 -0400

    Make unzip quiet.

--- Scripts/runzip.sh
+++ Scripts/runzip.sh
@@ -14,21 +14,21 @@ processed=0
 
 # Unziping
 find ./ -type f | grep "\.zip" |
-while read line          # As long as there is another line to read ...
+while read filename          # As long as there is another line to read ...
 do
-	dir=$(dirname "$line")
+	dir=$(dirname "$filename")
 	if [ "$2" = "commit" ]
 	then
-		unzip -o "$line" -d "$dir"
+		unzip -q -o "$filename" -d "$dir"
 		processed=$((processed+1))
 		if [ "$?" -eq 0 ] # Get the status of command executed.
 		then
-			printf "[$line][${green}OK${no_color}]"
+			printf "[$filename][${green}OK${no_color}]\n"
 		else
-			printf "[$line][${red}NOK${no_color}]"
+			printf "[$filename][${red}NOK${no_color}]\n"
 	    fi
 	else
-		echo "unzip -o $line -d $dir"
+		echo "unzip -o $filename -d $dir"
 	fi
 done
 
