d35a7fe5fdb87721ace41f4409db75398dd41526
Xuan Ngo <xuanngo@users.noreply.github.com>
2015-01-06 10:10:20 -0500

    Display directory size.

---
+++ Scripts/windows/dsize.bat
@@ -0,0 +1,22 @@
+:: Description: Display current directory size and its subdirectories up to 4 level deep.
+:: Require: du.exe, sort.exe
+:: Version: 1.0
+
+@ECHO OFF
+
+SET Unique=%date%_%time%
+SET Unique=%Unique:/=.%
+SET Unique=%Unique::=.%
+SET Unique=%Unique: =%
+SET Unique=%Unique:Mon=%
+SET Unique=%Unique:Tue=%
+SET Unique=%Unique:Wed=%
+SET Unique=%Unique:Thu=%
+SET Unique=%Unique:Fri=%
+SET Unique=%Unique:Sat=%
+SET Unique=%Unique:Sun=%
+ 
+
+du --max-depth=4 --human-readable > dsize_%Unique%.txt
+
+SET Unique=&::
