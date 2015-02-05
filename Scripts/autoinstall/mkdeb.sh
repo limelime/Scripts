f7df708be8eca54200e1cac38e5e2cc7be9133d7
xuanngo <xuanngo@users.noreply.github.com>
2015-02-05 09:58:39 -0500

    update.

--- Scripts/autoinstall/mkdeb.sh
+++ Scripts/autoinstall/mkdeb.sh
@@ -5,14 +5,16 @@
 #	-https://wiki.debian.org/DebianInstaller/Preseed/EditIso
 #	-https://codeghar.wordpress.com/2011/12/14/automated-customized-debian-installation-using-preseed/
 #	-http://blog.ericwhite.ca/articles/2009/11/unattended-debian-lenny-install/
-#	-http://www.hps.com/~tpg/notebook/autoinstall.php
+#	-http://www.hps.com/~tpg/notebook/autoinstall.php (A lot of tips)
 #	-http://users.telenet.be/mydotcom/howto/boot/boot_linux.htm
+#	-https://forums.virtualbox.org/viewtopic.php?f=10&t=46798
+#	-https://lists.debian.org/debian-boot/2012/01/msg00016.html (isolinux)
 
 
 # Manual input variables
 ISO_BASE_DIR=/media/apps/iso/debnetinst
-PRESEED_FILE=preseed/cust-deb-preseed.cfg
-#PRESEED_FILE=preseed/test-preseed.cfg
+#PRESEED_FILE=preseed/cust-deb-preseed.cfg
+PRESEED_FILE=preseed/test-preseed.cfg
 
 DATE_STRING=`date +"%Y-%m-%d_%0k.%M.%S"`
 OUTPUT_ISO=/media/sf_vm_sharedfolder/cust-deb-${DATE_STRING}.iso
@@ -48,8 +50,9 @@ rm -rf ${TMP_DIR}
 cd ${ISO_BASE_DIR}
 cp md5sum.txt md5sum.txt.bck
 md5sum `find -follow -type f` > md5sum.txt
-# find: File system loop detected; `./debian' is part of the same file system loop as `.'.
 cd ..
+# find: File system loop detected; `./debian' is part of the same file system loop as `.'.
+# md5sum `find ! -name "md5sum.txt" ! -path "./isolinux/*" -follow -type f` > md5sum.txt
 
 # Make ISO
 rm -f ${OUTPUT_ISO}
@@ -59,3 +62,7 @@ genisoimage  -r -V "cust-deb-iso" -cache-inodes -J -l -b isolinux/isolinux.bin -
 # Display info
 echo "Created ${OUTPUT_ISO} with ${PRESEED_FILE}."
 ls -al ${OUTPUT_ISO}
+
+# Install packages:
+# d-i pkgsel/include string ssh rsync initrd-tools cramfsprogs lzop
+
