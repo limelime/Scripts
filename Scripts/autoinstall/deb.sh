a7e5134f1821bed27315deadd362f97b6370058c
xuanngo <xuanngo@users.noreply.github.com>
2015-02-06 17:47:12 -0500

    update all.

--- Scripts/autoinstall/deb.sh
+++ Scripts/autoinstall/deb.sh
@@ -3,19 +3,14 @@
 # Author: Xuan Ngo
 
 MOD_ISO_DIR=/media/sf_vm_sharedfolder/moddebdir
-#ISO_FILE_PATH=/media/sf_vm_sharedfolder/debian-7.8.0-i386-netinst.iso
 ISO_FILE_PATH=/media/sf_vm_sharedfolder/debian-7.8.0-i386-CD-1.iso
-./extract-deb-iso.sh ${ISO_FILE_PATH} ${MOD_ISO_DIR}
-
-PRESEED_FILE_PATH=preseed/deb-7.8.0-i386-CD-1-preseed.cfg
-OUTPUT_ISO_DIR=/media/sf_vm_sharedfolder
-#./mkdeb.sh ${MOD_ISO_DIR} ${PRESEED_FILE_PATH} ${OUTPUT_ISO_DIR}
-
+#./extract-deb-iso.sh ${ISO_FILE_PATH} ${MOD_ISO_DIR}
 
 # Copy post-install folder to root path of CD-ROM.
 cp -R preseed/post-install/ ${MOD_ISO_DIR}
 
-
-
+PRESEED_FILE_PATH=preseed/deb-7.8.0-i386-CD-1-preseed.cfg
+OUTPUT_ISO_DIR=/media/sf_vm_sharedfolder
 YOUR_ISOLINUX_CFG=preseed/isolinux.cfg
 ./mk-deb-iso.sh ${MOD_ISO_DIR} ${PRESEED_FILE_PATH} ${OUTPUT_ISO_DIR} ${YOUR_ISOLINUX_CFG}
+#./mkdeb.sh ${MOD_ISO_DIR} ${PRESEED_FILE_PATH} ${OUTPUT_ISO_DIR}
