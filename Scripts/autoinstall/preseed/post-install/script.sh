3dc23282c22633b56227cb41ed09fa0339fb5dbf
xuanngo <xuanngo@users.noreply.github.com>
2015-02-06 16:56:24 -0500

    Install into multiple partitions.

--- Scripts/autoinstall/preseed/post-install/script.sh
+++ Scripts/autoinstall/preseed/post-install/script.sh
@@ -1,1 +1,8 @@
-echo "done" > /root/xuan_done.txt
+#!/bin/sh
+# Description: 
+# Author: Xuan Ngo
+
+### Change console resolution.
+echo "GRUB_GFXMODE=1024x768x32" >> /etc/default/grub
+echo "GRUB_GFXPAYLOAD_LINUX=keep" >> /etc/default/grub
+update-grub
