ca62443e7432e7c5e4507ee95be2c53ea461e874
xuanngo <xuanngo@users.noreply.github.com>
2015-02-09 11:25:32 -0500

    Disable cdrom in sources.list.

--- Scripts/autoinstall/preseed/post-install/after-install.sh
+++ Scripts/autoinstall/preseed/post-install/after-install.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Description: 
+# Description: This script will run after the installation from the ISO is completed.
 # Author: Xuan Ngo
 
 ### Change console resolution.
@@ -7,5 +7,9 @@ echo "GRUB_GFXMODE=1024x768x32" >> /etc/default/grub
 echo "GRUB_GFXPAYLOAD_LINUX=keep" >> /etc/default/grub
 update-grub
 
-### Add .bashrc
+### Add .bashrc.
 cp /root/post-install/bashrc_root /root/.bashrc
+
+### Replace sources.list
+# Disable the cdrom path from the apt sources.list automatically.
+sed -i 's/^deb cdrom/#deb/' /etc/apt/sources.list
