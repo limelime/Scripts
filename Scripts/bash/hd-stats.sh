71cf2c0583f8d9b3e63cdb85f7124d9279546171
limelime <limelime@users.noreply.github.com>
2015-02-27 11:34:22 -0500

    Add hard drive info script.

---
+++ Scripts/bash/hd-stats.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#########################################################
+# Description: Display hard drive info(device, size, 
+#               model, serial number)
+# Author: Xuan Ngo
+# Usage:
+#   ./hd-stats.sh
+#########################################################
+
+#### To handle line with spaces.
+SAVE_IFS=$IFS
+IFS=$(echo -en "\n\b")
+
+for fdisk_data in `fdisk -l | grep 'Disk /dev/' | sed 's/^Disk //'`;
+do
+  device=$(echo ${fdisk_data}| sed 's/:.*//' )
+  hd_info=$(hdparm -i $device 2> /dev/null | grep 'Model')
+  
+	if [ ${#hd_info} -eq 0 ]; then
+	  hd_info=" ERROR: hdparm -i ${device}"
+	fi
+
+  echo "${fdisk_data},${hd_info}"
+
+done
+
+### Restore IFS
+IFS=$SAVE_IFS
+
+  
+  
+  
