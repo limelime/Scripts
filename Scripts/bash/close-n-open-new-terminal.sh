0db513f22ba1fb1e03f062ec968a3879c5d5ad16
limelime <limelime@users.noreply.github.com>
2015-03-20 10:36:32 -0400

    update comment.

--- Scripts/bash/close-n-open-new-terminal.sh
+++ Scripts/bash/close-n-open-new-terminal.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Description: Close current terminal and open a terminal.
+# Description: Close current terminal and open a new terminal.
 # Author: Xuan Ngo
 # Require: nohup, xfce4-terminal
 
