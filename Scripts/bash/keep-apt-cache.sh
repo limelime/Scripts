f5ec31ff3ea3e077db8a22548acb988f768a30cc
limelime <limelime@users.noreply.github.com>
2015-05-06 20:01:37 +0000

    Generate package list.

--- Scripts/bash/keep-apt-cache.sh
+++ Scripts/bash/keep-apt-cache.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # Description: Copy apt cache archive directory to another directory.
+# Requirements: aptitude -y install dpkg-dev
 # Make this script run at shutdown:
 # $ chmod +x keep-apt-cache.sh
 # $ cp keep-apt-cache.sh /etc/init.d/
@@ -19,12 +20,18 @@ then
   ARCH="64bit"
 fi
 
+### Copy *.deb packages to destination directory.
 DEST_APT_CACHE_ARCHIVES_DIR=${DEST_BASEDIR}/"${DEB_VERSION}-${ARCH}"
 mkdir -p ${DEST_APT_CACHE_ARCHIVES_DIR}
-
 rsync -a "${SRC_APT_CACHE_ARCHIVES_DIR}/" "${DEST_APT_CACHE_ARCHIVES_DIR}"
-
 echo "Copied ${SRC_APT_CACHE_ARCHIVES_DIR} to ${DEST_APT_CACHE_ARCHIVES_DIR}."
+
+
+### Generate package list to be used as repository.
+cd "${DEST_APT_CACHE_ARCHIVES_DIR}"
+dpkg-scanpackages . /dev/null | gzip -9c > Packages.gz
+
+
 
 ### References
 # https://www.digitalocean.com/community/tutorials/how-to-use-rsync-to-sync-local-and-remote-directories-on-a-vps
