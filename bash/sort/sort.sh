9b5d14cfd127b2e50fe5e902ca192f8e5a8107a9
limelime <limelime@users.noreply.github.com>
2015-12-31 13:24:23 -0500

    Add sort example.

---
+++ bash/sort/sort.sh
@@ -0,0 +1,15 @@
+INPUT_FILE="people.txt"
+
+CMD="cat ${INPUT_FILE}"
+echo "${CMD}"
+eval "${CMD}" | sed 's/^/#   /'; echo ""
+
+echo "# Sort by the 3rd column"
+SORT_CMD="sort -t ';' -k3,3 ${INPUT_FILE}"
+echo "${SORT_CMD}"
+eval "${SORT_CMD}" | sed 's/^/#   /'; echo ""
+
+echo "# Sort by 2nd, 1st and then 3rd column"
+SORT_CMD="sort -t ';' -k2,2 -k1,1 -k3,3 ${INPUT_FILE}"
+echo "${SORT_CMD}" 
+eval "${SORT_CMD}" | sed 's/^/#   /'; echo ""
