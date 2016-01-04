2812d8ab19202e3f835392da6beffe604fc43e24
limelime <limelime@users.noreply.github.com>
2016-01-04 13:06:52 -0500

    Sort by number.

--- bash/sort/sort.sh
+++ bash/sort/sort.sh
@@ -5,11 +5,11 @@ echo "${CMD}"
 eval "${CMD}" | sed 's/^/#   /'; echo ""
 
 echo "# Sort by the 3rd column"
-SORT_CMD="sort -t ';' -k3,3 ${INPUT_FILE}"
+SORT_CMD="sort -t ';' -k3,3n ${INPUT_FILE}"
 echo "${SORT_CMD}"
 eval "${SORT_CMD}" | sed 's/^/#   /'; echo ""
 
 echo "# Sort by 2nd, 1st and then 3rd column"
-SORT_CMD="sort -t ';' -k2,2 -k1,1 -k3,3 ${INPUT_FILE}"
+SORT_CMD="sort -t ';' -k2,2 -k1,1 -k3,3n ${INPUT_FILE}"
 echo "${SORT_CMD}" 
 eval "${SORT_CMD}" | sed 's/^/#   /'; echo ""
