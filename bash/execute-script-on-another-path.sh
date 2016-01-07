cc73335856ad31982ae64a380764c261bfdb35ca
limelime <limelime@users.noreply.github.com>
2016-01-07 11:56:14 -0500

    Add.

---
+++ bash/execute-script-on-another-path.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+set -e
+# Description: Execute script located on another path and return to the current working directory.
+#   Solution: Use subshell, i.e. ().
+
+
+TMP_DIR="$0_tmp-test-folder983"
+mkdir -p "${TMP_DIR}"
+
+touch "${TMP_DIR}/file-from-another-path.txt"
+
+
+( 
+  cd "${TMP_DIR}"; echo "List the other directory:"
+  ls *
+)
+
+echo ""
+
+echo "List current directory:"
+ls $0
