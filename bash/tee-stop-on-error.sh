f2bb2f002f85c555eb08fcdcb55bfd7cc8eaf6e3
limelime <limelime@users.noreply.github.com>
2016-01-22 14:38:00 -0500

    Add tee to stop on error.

---
+++ bash/tee-stop-on-error.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+# Description: Use 'set -o pipefail' to ensure script stop if anything fail through the pipe
+
+set -o pipefail
+echo 'hello error'; exit 1 | tee some.log
+
+echo 'Continue on error: This line should not be displayed.'
