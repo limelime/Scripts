611bdc9e38a12297aa207d11a56571be8df7a6e9
limelime <limelime@users.noreply.github.com>
2017-02-07 11:36:01 -0500

    Redirect all output to tee.

--- bash/tee-stop-on-error.sh
+++ bash/tee-stop-on-error.sh
@@ -3,6 +3,6 @@ set -e
 # Description: Use 'set -o pipefail' to ensure script stop if anything fail through the pipe
 
 set -o pipefail
-echo 'hello error'; exit 1 | tee some.log
+echo 'hello error'; exit 1 2>&1 | tee some.log
 
 echo 'Continue on error: This line should not be displayed.'
